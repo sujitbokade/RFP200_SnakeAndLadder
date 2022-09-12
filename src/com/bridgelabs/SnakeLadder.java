066c4e21af917550609668eb4a03930356a9f4e5
sujit <sujitbokade2014@gmail.com>
2022-09-12 17:40:57 +0530

    [Sujit]Welcome Message

---
+++ src/com/bridgelabs/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabs;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake & Ladder Game");
+        int startPosition = 0;
+    }
+}
