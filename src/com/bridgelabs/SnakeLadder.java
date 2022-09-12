55d18e0015c3640e156a759210c40cf945647cc8
sujit <sujitbokade2014@gmail.com>
2022-09-12 17:48:08 +0530

    [Sujit]Roll The Dice

--- src/com/bridgelabs/SnakeLadder.java
+++ src/com/bridgelabs/SnakeLadder.java
@@ -4,5 +4,7 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake & Ladder Game");
         int startPosition = 0;
+        int dice = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println(dice);
     }
 }
